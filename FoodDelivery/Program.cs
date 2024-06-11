5448e78c007157d8b732622c6787142e3ca2b765
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-11 23:04:31 +0300

    Initial commit

---
+++ FoodDelivery/Program.cs
@@ -0,0 +1,25 @@
+﻿class FoodDelivery
+{
+    public static void Main(string[] args)
+    {
+        int chickenCount = int.Parse(Console.ReadLine());
+        int fishCount = int.Parse(Console.ReadLine());
+        int vegetarianCount = int.Parse(Console.ReadLine());
+        
+        double chickenMenuPrice=10.35;
+
+        double fishMenuPrice=12.40;
+
+        double vegetarianMenuPrice = 8.15;
+
+        double delivery = 2.50;
+
+        double bill = chickenCount * chickenMenuPrice + fishCount * fishMenuPrice +
+                      vegetarianCount * vegetarianMenuPrice;
+        double dessert =  bill * 0.20;
+
+        double totalAmount = bill + dessert+delivery;
+        
+        Console.WriteLine(totalAmount);
+    }
+    }
